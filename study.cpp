87010f5696d062753db0761503850eaf94b43e41
maksympetliukh <maksympetliukh@icloud.com>
2025-06-19 10:59:41 +0200

    1

--- study.cpp
+++ study.cpp
@@ -1,2 +1,34 @@
 #include <iostream>
 
+class Car {
+    public:
+    std::string brand;
+    std::string model;
+    unsigned price;
+    unsigned year;
+
+    Car(std::string b, std::string m, unsigned p, unsigned y){
+        brand = b;
+        model = m; 
+        price = p;
+        year = y;
+    }
+
+    Car (const Car &p){
+        brand = p.brand;
+        model = "Corolla-S";
+        price = p.price + 5000;
+        year = p.year + 4;
+    }
+
+    void show(){
+        std::cout << "\n" << brand << "\n" << model << "\nPrice: " << price << "\nRelease year: " << year << std::endl;
+    }
+};
+
+int main(){
+    Car toyota2021 {"Toyota", "Corolla", 20000, 2021};
+    toyota2021.show();
+    Car toyota2025 {toyota2021};
+    toyota2025.show();
+}
