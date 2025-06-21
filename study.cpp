66586b9b3ba7ebf3ec289321ced8f2828109a48e
maksympetliukh <maksympetliukh@icloud.com>
2025-06-21 12:44:58 +0200

    3

--- study.cpp
+++ study.cpp
@@ -10,17 +10,17 @@ class Heir {
     friend void show (Heir& h1, Heir& h2);
 };
 
-bool order_of_succession(Heir& h1, Heir& h2){
+bool order_of_succession (Heir& h1, Heir& h2){
     return h1.age > h2.age;
 }
 
-void show(Heir& h1, Heir& h2){
+void show (Heir& h1, Heir& h2){
     if(order_of_succession(h1, h2)){
         std::cout << h1.name << " will be the next king because he is older than " << h2.name << std::endl;
     }
-    else if(h1.age == h2.age){
+    else if (h1.age == h2.age){
         std::cout << "Since the sons are the same age, the successor is determined by primogeniture: ";
-        if(h1.primogeniture < h2.primogeniture && h1.primogeniture != h2.primogeniture){
+        if (h1.primogeniture < h2.primogeniture && h1.primogeniture != h2.primogeniture){
             std::cout << h1.name << " will be the next king" << std::endl;
         }else{
             std::cout << h2.name << " will be the next king" << std::endl;
@@ -33,7 +33,7 @@ int main(){
     Heir charles {"Charles", 15, 2};
     Heir george {"George", 15, 1};
 
-    show(arthur, charles);
-    show(arthur, george);
-    show(charles, george);
+    show (arthur, charles);
+    show (arthur, george);
+    show (charles, george);
 }
