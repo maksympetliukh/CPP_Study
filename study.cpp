871b1853f9c0440c5fe4fe98c0a55cafe787eb86
maksympetliukh <maksympetliukh@icloud.com>
2025-06-23 15:54:36 +0200

    private inheritance

--- study.cpp
+++ study.cpp
@@ -1,39 +1,63 @@
 #include <iostream>
 
-class Heir {
-    std::string name;
-    unsigned age;
-    unsigned primogeniture;
-    public:
-    Heir (std::string n, unsigned a, unsigned p): name(n), age(a), primogeniture(p) {};
-    friend bool order_of_succession (Heir& h1, Heir& h2);
-    friend void show (Heir& h1, Heir& h2);
+class Animal{
+    std::string animal;
+    std::string rase;
+    unsigned weight;
+    unsigned price;
+public:
+    Animal (std::string a, std::string r, unsigned w, unsigned p):
+        animal(a), rase(r),
+        weight(w), price(p) {};
+        void show () const {
+            std::cout << animal << "\nRase: " << rase << "\nWeigth: " << weight  << " kg" << "\nPrice: " << price << " USD"<< std::endl; 
+        }
+
+    std::string get_animal () const {return animal;}
+    std::string get_rase () const {return rase;}
+    unsigned get_weight () const {return weight;}
+    unsigned get_price () const {return price;}
 };
 
-bool order_of_succession (Heir& h1, Heir& h2){
-    return h1.age > h2.age;
-}
+class Cat: private Animal {
+public:
+    Cat (std::string a, std::string r, unsigned w, unsigned p): Animal(a, r, w, p) {};
 
-void show (Heir& h1, Heir& h2){
-    if(order_of_succession(h1, h2)){
-        std::cout << h1.name << " will be the next king because he is older than " << h2.name << std::endl;
+    using Animal::get_animal;
+    using Animal::get_rase;
+    using Animal::get_weight;
+    using Animal::get_price;
+
+    void show () const {
+        std::cout << "Animal: " << get_animal () << std::endl;
+        std::cout << "Rase: " << get_rase ()<< std::endl;
+        std::cout << "Weight: " << get_weight ()<< " kg" << std::endl;
+        std::cout << "Price: " << get_price ()<< " USD" << std::endl;
     }
-    else if (h1.age == h2.age){
-        std::cout << "Since the sons are the same age, the successor is determined by primogeniture: ";
-        if (h1.primogeniture < h2.primogeniture && h1.primogeniture != h2.primogeniture){
-            std::cout << h1.name << " will be the next king" << std::endl;
-        }else{
-            std::cout << h2.name << " will be the next king" << std::endl;
-        }
+};  
+
+class Dog: private Animal {
+public:
+    Dog (std::string a, std::string r, unsigned w, unsigned p): Animal(a, r, w, p) {};
+
+    using Animal::get_animal;
+    using Animal::get_rase;
+    using Animal::get_weight;
+    using Animal::get_price;
+
+    void show () const {
+        std::cout << "\n" << "Animal: " << get_animal () << std::endl;
+        std::cout << "Rase: " << get_rase () << std::endl;
+        std::cout << "Weight: " << get_weight () << " kg" << std::endl;
+        std::cout << "Price: " << get_price () << " USD" << std::endl;
     }
-}
+}; 
+
 
 int main(){
-    Heir arthur {"Arthur", 19, 1};
-    Heir charles {"Charles", 15, 2};
-    Heir george {"George", 15, 1};
+    Cat cat {"Cat", "Manx", 4, 150};
+    Dog dog {"Dog", "Doberman", 20, 250};
 
-    show (arthur, charles);
-    show (arthur, george);
-    show (charles, george);
+    cat.show ();
+    dog.show ();
 }
